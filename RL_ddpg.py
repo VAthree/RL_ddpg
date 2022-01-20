d857961376d24c4a9a9d1e3af3d197f5f18b58b5
VAthree <56064364+VAthree@users.noreply.github.com>
2022-01-20 12:55:20 +0800

    original DDPG

---
+++ RL_ddpg.py
@@ -0,0 +1,264 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.tensorboard import SummaryWriter
+
+import argparse
+from distutils.util import strtobool
+import collections
+import numpy as np
+import gym
+import pybullet_envs
+from gym.wrappers import TimeLimit, Monitor
+from gym.spaces import Discrete, Box, MultiBinary, MultiDiscrete, Space
+import time
+import random
+import os
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='DDPG agent')
+    # Common arguments
+    parser.add_argument('--exp-name', type=str, default=os.path.basename(__file__).rstrip(".py"),
+                        help='the name of this experiment')
+    parser.add_argument('--gym-id', type=str, default="Pendulum-v0", #  MountainCarContinuous-v0
+                        help='the id of the gym environment')
+    parser.add_argument('--learning-rate', type=float, default=3e-4,
+                        help='the learning rate of the optimizer')
+    parser.add_argument('--seed', type=int, default=1,
+                        help='seed of the experiment')
+    parser.add_argument('--episode-length', type=int, default=0,
+                        help='the maximum length of each episode')
+    parser.add_argument('--total-timesteps', type=int, default=200000,
+                        help='total timesteps of the experiments')
+    parser.add_argument('--torch-deterministic', type=lambda x:bool(strtobool(x)), default=True, nargs='?', const=True,
+                        help='if toggled, `torch.backends.cudnn.deterministic=False`')
+    parser.add_argument('--cuda', type=lambda x:bool(strtobool(x)), default=True, nargs='?', const=True,
+                        help='if toggled, cuda will not be enabled by default')
+    parser.add_argument('--track', type=lambda x:bool(strtobool(x)), default=False, nargs='?', const=True,
+                        help='run the script in production mode and use wandb to log outputs')
+    parser.add_argument('--capture-video', type=lambda x:bool(strtobool(x)), default=False, nargs='?', const=True,
+                        help='weather to capture videos of the agent performances (check out `videos` folder)')
+    parser.add_argument('--wandb-project-name', type=str, default="cleanRL",
+                        help="the wandb's project name")
+    parser.add_argument('--wandb-entity', type=str, default=None,
+                        help="the entity (team) of wandb's project")
+    
+    # Algorithm specific arguments
+    parser.add_argument('--buffer-size', type=int, default=int(1e6),
+                         help='the replay memory buffer size')
+    parser.add_argument('--gamma', type=float, default=0.99,
+                        help='the discount factor gamma')
+    parser.add_argument('--tau', type=float, default=0.005,
+                        help="target smoothing coefficient (default: 0.005)")
+    parser.add_argument('--max-grad-norm', type=float, default=0.5,
+                        help='the maximum norm for the gradient clipping')
+    parser.add_argument('--batch-size', type=int, default=256,
+                        help="the batch size of sample from the reply memory")
+    parser.add_argument('--exploration-noise', type=float, default=0.1,
+                        help='the scale of exploration noise')
+    parser.add_argument('--learning-starts', type=int, default=25e3,
+                        help="timestep to start learning")
+    parser.add_argument('--policy-frequency', type=int, default=2,
+                        help="the frequency of training policy (delayed)")
+    parser.add_argument('--noise-clip', type=float, default=0.5,
+                         help='noise clip parameter of the Target Policy Smoothing Regularization')
+    parser.add_argument('--path', type=str,
+                         help='model save path')                  
+    args = parser.parse_args()
+    if not args.seed:
+        args.seed = int(time.time())
+
+# Initialize the gym game environment
+experiment_name = f"{args.gym_id}"
+args.path = experiment_name
+writer = SummaryWriter(f"runs/{experiment_name}")
+writer.add_text('hyperparameters', "|param|value|\n|-|-|\n%s" % (
+        '\n'.join([f"|{key}|{value}|" for key, value in vars(args).items()])))
+if args.track:
+    import wandb
+    wandb.init(project=args.wandb_project_name, entity=args.wandb_entity, sync_tensorboard=True, config=vars(args), name=experiment_name, monitor_gym=True, save_code=True)
+    writer = SummaryWriter(f"/tmp/{experiment_name}")
+
+
+# fixed random seed
+device = torch.device('cuda' if torch.cuda.is_available() and args.cuda else 'cpu')
+env = gym.make(args.gym_id)
+random.seed(args.seed)
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+torch.backends.cudnn.deterministic = args.torch_deterministic
+env.seed(args.seed)
+# Action space and observation space of the instantiated environment
+# Includes fitting and definition of noise functions
+env.action_space.seed(args.seed)
+env.observation_space.seed(args.seed)
+# respect the default timelimit
+assert isinstance(env.action_space, Box), "only continuous action space is supported"
+assert isinstance(env, TimeLimit) or int(args.episode_length), "the gym env does not have a built in TimeLimit, please specify by using --episode-length"
+if isinstance(env, TimeLimit):
+    if int(args.episode_length):
+        env._max_episode_steps = int(args.episode_length)
+    args.episode_length = env._max_episode_steps
+else:
+    env = TimeLimit(env, int(args.episode_length))
+if args.capture_video:
+    env = Monitor(env, f'videos/{experiment_name}')
+
+# experience pool structure
+class ReplayBuffer():
+    # Initialize the experience pool
+    def __init__(self, buffer_limit):
+        self.buffer = collections.deque(maxlen=buffer_limit)
+    # Push experience into the experience pool
+    def put(self, transition):
+        self.buffer.append(transition)
+    # Mini-batch sampling from experience pools
+    def sample(self, n):
+        # Randomly sample a batch of samples
+        mini_batch = random.sample(self.buffer, n)
+        s_lst, a_lst, r_lst, s_prime_lst, done_mask_lst = [], [], [], [], []
+        
+        for transition in mini_batch:
+            s, a, r, s_prime, done_mask = transition
+            s_lst.append(s)
+            a_lst.append(a)
+            r_lst.append(r)
+            s_prime_lst.append(s_prime)
+            done_mask_lst.append(done_mask)
+
+        return np.array(s_lst), np.array(a_lst), \
+               np.array(r_lst), np.array(s_prime_lst), \
+               np.array(done_mask_lst)
+
+# Critic network structure
+class QNetwork(nn.Module):
+    def __init__(self, env):
+        # The network structure consists of three fully connected layers 
+        # The output size of the final network is 1:Q value
+        super(QNetwork, self).__init__()
+        self.fc1 = nn.Linear(
+            np.array(env.observation_space.shape).prod()+np.prod(env.action_space.shape), 256)
+        self.fc2 = nn.Linear(256, 256)
+        self.fc3 = nn.Linear(256, 1)
+    # Critic network for forward propagation
+    def forward(self, x, a, device):
+        # Use Relu as activation function
+        x = torch.Tensor(x).to(device)
+        x = torch.cat([x, a], 1)    
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+# Actor network structure
+class Actor(nn.Module):
+    def __init__(self, env):
+        # The network structure consists of three fully connected layers
+        super(Actor, self).__init__()
+        self.fc1 = nn.Linear(np.array(env.observation_space.shape).prod(), 256)
+        self.fc2 = nn.Linear(256, 256)
+        self.fc_mu = nn.Linear(256, np.prod(env.action_space.shape))
+    # Actor network for forward propagation
+    def forward(self, x, device):
+        # Use Relu as activation function
+        x = torch.Tensor(x).to(device)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        return torch.tanh(self.fc_mu(x))
+
+def linear_schedule(start_sigma: float, end_sigma: float, duration: int, t: int):
+    slope =  (end_sigma - start_sigma) / duration
+    return max(slope * t + start_sigma, end_sigma)
+max_action = float(env.action_space.high[0])
+# Instantiate the experience pool
+rb = ReplayBuffer(args.buffer_size)
+# Instantiate critic's current network and target network
+actor = Actor(env).to(device)
+qf1 = QNetwork(env).to(device)
+# Instantiate the actor's current network and target network
+qf1_target = QNetwork(env).to(device)
+target_actor = Actor(env).to(device)
+# target network loading environment status
+target_actor.load_state_dict(actor.state_dict())
+qf1_target.load_state_dict(qf1.state_dict())
+# Define the optimizer for the network
+q_optimizer = optim.Adam(list(qf1.parameters()), lr=args.learning_rate)
+actor_optimizer = optim.Adam(list(actor.parameters()), lr=args.learning_rate)
+# Define the loss function of the Critic network
+# The loss function of the Actor network is calculated separately later
+loss_fn = nn.MSELoss()
+# Play in the gym's continuous action space
+obs = env.reset()
+episode_reward = 0
+
+for global_step in range(args.total_timesteps):
+    # Agent chooses action A
+    # In the initial stage, it is more inclined to explore, randomly choosing actions based on the environment
+    # After learning for a period of time, it is more inclined to use and select actions based on the network
+    
+    if global_step < args.learning_starts:
+        action = env.action_space.sample()
+    else:
+        action = actor.forward(obs.reshape((1,)+obs.shape), device)
+        action = (
+            action.tolist()[0]
+            + np.random.normal(0, max_action * args.exploration_noise, size=env.action_space.shape[0])
+        ).clip(env.action_space.low, env.action_space.high)
+
+    # Perform the chosen action, move to a new state S', get the reward R
+    next_obs, reward, done, info = env.step(action)
+    episode_reward += reward
+    
+    # Put experience into the experience pool
+    rb.put((obs, action, reward, next_obs, done))
+    # Train the network below
+    if global_step > args.learning_starts:
+        # Sample mini-batches of experience from the experience pool
+        s_obs, s_actions, s_rewards, s_next_obses, s_dones = rb.sample(args.batch_size)
+        # Calculate the Q value of the current target
+        with torch.no_grad():
+            next_state_actions = (
+                target_actor.forward(s_next_obses, device)
+            ).clamp(env.action_space.low[0], env.action_space.high[0])
+            qf1_next_target = qf1_target.forward(s_next_obses, next_state_actions, device)
+            next_q_value = torch.Tensor(s_rewards).to(device) + (1 - torch.Tensor(s_dones).to(device)) * args.gamma * (qf1_next_target).view(-1)
+        # Calculate the error of Critic's current network using the mean squared loss function
+        qf1_a_values = qf1.forward(s_obs, torch.Tensor(s_actions).to(device), device).view(-1)
+        qf1_loss = loss_fn(qf1_a_values, next_q_value)
+
+        # Update the parameter w of the Critic current network through gradient back-propagation of the neural network
+        q_optimizer.zero_grad()
+        qf1_loss.backward()
+        nn.utils.clip_grad_norm_(list(qf1.parameters()), args.max_grad_norm)
+        q_optimizer.step()
+        # Calculate the loss of the Actor current network
+        if global_step % args.policy_frequency == 0:
+            actor_loss = -qf1.forward(s_obs, actor.forward(s_obs, device), device).mean()
+            # Update the parameter of the Actor current network through gradient back-propagation of the neural network
+            actor_optimizer.zero_grad()
+            actor_loss.backward()
+            nn.utils.clip_grad_norm_(list(actor.parameters()), args.max_grad_norm)
+            actor_optimizer.step()
+
+            # Update target network parameters based on a certain frequency
+            for param, target_param in zip(actor.parameters(), target_actor.parameters()):
+                target_param.data.copy_(args.tau * param.data + (1 - args.tau) * target_param.data)
+            for param, target_param in zip(qf1.parameters(), qf1_target.parameters()):
+                target_param.data.copy_(args.tau * param.data + (1 - args.tau) * target_param.data)
+
+        if global_step % 100 == 0:
+            writer.add_scalar("losses/qf1_loss", qf1_loss.item(), global_step)
+            writer.add_scalar("losses/actor_loss", actor_loss.item(), global_step)
+
+    obs = next_obs
+
+    if done:
+        print(f"global_step={global_step}, episode_reward={episode_reward}")
+        writer.add_scalar("charts/episodic_return", episode_reward, global_step)
+        obs, episode_reward = env.reset(), 0
+    # 模型保存
+    if global_step % 100 == 0:
+        torch.save(actor.state_dict(),'runs/'+args.path+'/checkpoint.pt')
+
+env.close()
+writer.close()
